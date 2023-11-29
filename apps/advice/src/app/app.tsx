7a33f56ad22cc88c68e39db2ec4e5202874b9247
Pawan <pawanroy0066@gmail.com>
2023-11-29 13:44:47 +0530

    Advice Project fetching advices from API

--- apps/advice/src/app/app.tsx
+++ apps/advice/src/app/app.tsx
@@ -1,13 +1,40 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
+import {
+  useEffect,
+  useState
+} from 'react';
 
 export function App() {
+  const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((value) => value + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <div>
-      <NxWelcome title="advice" />
+      <h1>Hello World</h1>
+      <h2>{advice}</h2>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </div>
+  );
+}
+
+function Message(prop: { count: number }) {
+  return (
+    <p>
+      You have read <strong>{prop.count}</strong> pieces of advice.
+    </p>
   );
 }
 
